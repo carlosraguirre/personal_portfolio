d897f3ba0e8b05a448f03bdfc53fb2ab5937baec
Carlos Aguirre <craguirre952@gmail.com>
2021-11-04 00:20:07 -0400

    add google fonts links

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,11 @@
 <template>
   <body>
+    <!-- Google Fonts -->
+    <link
+      href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+      rel="stylesheet"
+    />
+    <link href="/assets/css/style.css" rel="stylesheet" />    
     <!-- ======= Hero Section ======= -->
     <section
       id="hero"
@@ -281,7 +287,7 @@
           <div class="section-title">
             <h2>Portfolio</h2>
             <p>
-              Check out some projects I have worked on for Actualize Coding Bootcamp and for fun.
+              Below are some projects I have worked on for coding bootcamp and for fun
             </p>
           </div>
 
